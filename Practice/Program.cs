ecbd921abc996a80519d3182c5f695ac62974093
Bhanureddy7 <95532817+Bhanureddy7@users.noreply.github.com>
2025-07-28 20:49:11 +0530

    console changed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -9,7 +9,7 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello C#");
         }
     }
 }

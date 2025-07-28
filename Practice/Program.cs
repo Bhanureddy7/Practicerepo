8f7e52df9e64f4e188816a00cae1e62134432bc5
Bhanureddy7 <95532817+Bhanureddy7@users.noreply.github.com>
2025-07-28 20:45:19 +0530

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Threading;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
